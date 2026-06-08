d9b328cf6c1031efbac5b9a77334f56f1dc9dc25
colbyrreed <colbyreed1million@gmail.com>
2026-06-08 12:05:24 -0500

    Add src/middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,29 @@
-// src/middleware.ts
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { updateSession } from "@/utils/supabase/middleware";
+import type { NextRequest } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
-  '/',
-  '/sign-in(.*)',
-  '/sign-up(.*)',
-  '/pricing',
-  '/api/webhooks(.*)',
-  '/api/stripe/webhook',
-])
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/pricing",
+  "/api/webhooks(.*)",
+  "/api/stripe/webhook",
+  "/api/cron(.*)",
+]);
 
-export default clerkMiddleware((auth, req) => {
+export default clerkMiddleware(async (auth, req: NextRequest) => {
+  // Refresh Supabase session
+  await updateSession(req);
+  // Protect private routes
   if (!isPublicRoute(req)) {
-    auth().protect()
+    await auth.protect();
   }
-})
+});
 
 export const config = {
   matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
+    "/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
   ],
-}
+};
